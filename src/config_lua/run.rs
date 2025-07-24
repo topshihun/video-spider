4d282b6c21463cbcd969caa4d4de3f316cccf19f
Wangyan <topshihun@foxmail.com>
2025-07-24 16:16:31 +0800

    change: test dir

--- src/config_lua/run.rs
+++ src/config_lua/run.rs
@@ -36,15 +36,15 @@ mod tests {
     
     #[test]
     fn test_lua_run() {
-        let path = Path::new("./tests/config-lua/simple-main.lua");
+        let path = Path::new("./tests/config_lua/simple-main.lua");
         let series = lua_run(&path, "mykey").unwrap();
         assert_eq!(series.name, "video_namemykey");
         assert_eq!(series.description, "description");
         assert_eq!(series.image.to_string(), "http://localhost/simple.png");
         assert_eq!(series.episodes.len(), 2);
-        assert_eq!(series.episodes.get(0).unwrap().name, "2");
-        assert_eq!(series.episodes.get(0).unwrap().addr.to_string(), "http://localhost/simple2.mp4");
-        assert_eq!(series.episodes.get(1).unwrap().name, "1");
-        assert_eq!(series.episodes.get(1).unwrap().addr.to_string(), "http://localhost/simple1.mp4");
+        assert_eq!(series.episodes.get(0).unwrap().name, "1");
+        assert_eq!(series.episodes.get(0).unwrap().addr.to_string(), "http://localhost/simple1.mp4");
+        assert_eq!(series.episodes.get(1).unwrap().name, "2");
+        assert_eq!(series.episodes.get(1).unwrap().addr.to_string(), "http://localhost/simple2.mp4");
     }
 }
