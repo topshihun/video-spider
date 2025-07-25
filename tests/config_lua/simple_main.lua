b002df01f57204fdef075a628005dfdf1e1b4b7d
Wangyan <topshihun@foxmail.com>
2025-07-25 17:17:16 +0800

    change: simple_main lua

--- tests/config_lua/simple_main.lua
+++ tests/config_lua/simple_main.lua
@@ -1,10 +1,15 @@
 function main(key)
   local video = {}
-  video["name"] = "video_name" .. key
-  video["description"] = "description"
-  video["image"] = "http://localhost/simple.png"
-  video["episodes"] = {}
-  video["episodes"]["1"] = "http://localhost/simple1.mp4"
-  video["episodes"]["2"] = "http://localhost/simple2.mp4"
+  video[1]["name"] = "video_name1" .. key
+  video[1]["description"] = "description"
+  video[1]["image"] = "http://localhost/simple.png"
+  video[1]["episodes"]["1"] = "http://localhost/simple1.mp4"
+  video[1]["episodes"]["2"] = "http://localhost/simple2.mp4"
+
+  video[2]["name"] = "video_name2" .. key
+  video[2]["description"] = "description"
+  video[2]["image"] = "http://localhost/simple.png"
+  video[2]["episodes"]["1"] = "http://localhost/simple1.mp4"
+  video[2]["episodes"]["2"] = "http://localhost/simple2.mp4"
   return video
 end
