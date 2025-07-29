96320e47277b2303abc6ffd24ca3be1399df9711
Wangyan <topshihun@foxmail.com>
2025-07-29 15:51:38 +0800

    remove: ffzy print

--- tests/config_lua/ffzy.lua
+++ tests/config_lua/ffzy.lua
@@ -7,15 +7,12 @@ local base_url = "http://api.ffzyapi.com/api.php/provide/vod/?"
 
 function main(keyword)
   local video_list_json = utils.http_get(base_url .. "ac=list&wd=" .. keyword)
-  print(video_list_json)
   local video_list = utils.json_parse(video_list_json)
   video_list = video_list["list"]
   local video_ids = ""
-  print(#video_list)
   for _, video in ipairs(video_list) do
     video_ids = video["vod_id"] .. "," .. video_ids
   end
-  print("video_ids:" .. video_ids)
   local video_details_json = utils.http_get(base_url .. "ac=detail&ids=" .. video_ids)
   local video_details = utils.json_parse(video_details_json)
   local series_arr = {}
@@ -26,11 +23,8 @@ function main(keyword)
     series_arr[i]["image"] = video_detail["vod_pic"]
     -- video urls
     series_arr[i]["episodes"] = {}
-    print(video_detail["vod_play_url"])
     local episode_arr_str = utils.string_split(video_detail["vod_play_url"], "#")
-    print(#episode_arr_str)
     for _, episode_str in ipairs(episode_arr_str) do
-      print(episode_str)
       local episode_key_val = utils.string_split(episode_str, "$")
       series_arr[i]["episodes"][episode_key_val[1]] = episode_key_val[2]
     end
